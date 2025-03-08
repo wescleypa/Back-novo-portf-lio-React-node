258d586717ad36a6c9c19613769800ea3fa33b28
Wescley Andrade <wescleypa.br@gmail.com>
2025-03-07 23:38:19 -0300

    SSL

--- index.js
+++ index.js
@@ -8,16 +8,21 @@ const cors = require('cors');
 const app = express();
 
 // Middleware para processar JSON no corpo da requisição
-app.use(express.json());
+app.use(express.json())
+
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/api.souwescley.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/api.souwescley.com/fullchain.pem'),
+};;
 
 // Configuração do CORS
 app.use(cors({
-  origin: 'http://localhost:3000', // Permite apenas requisições do frontend
+  origin: 'https://souwescley.com:3000', // Permite apenas requisições do frontend
   methods: ['GET', 'POST'], // Métodos permitidos
   credentials: true, // Permite envio de cookies e cabeçalhos de autenticação
 }));
 
-const server = http.createServer(app);
+const server = http.createServer(options, app);
 const io = new Server(server, {
   cors: {
     origin: '*',
